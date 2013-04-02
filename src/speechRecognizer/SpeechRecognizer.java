9a961b8f25cdda2b8c2d49e6a0b9d9355f037c2a
yeshwanth <yeshwanth43@gmail.com>
2013-04-02 11:53:16 +0530

    Recognizer Package Freezed with all configuration [UI Integration Pending]

--- src/speechRecognizer/SpeechRecognizer.java
+++ src/speechRecognizer/SpeechRecognizer.java
@@ -7,9 +7,9 @@ import edu.cmu.sphinx.util.props.ConfigurationManager;
 
 public class SpeechRecognizer {
 
-	public String recognizeSpeech() {
+	public void recognizeSpeech() {
 		ConfigurationManager cm = new ConfigurationManager(
-				SpeechRecognizer.class.getResource("helloworld.config.xml"));
+				"conf/commands.config.xml");
 		Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
 		recognizer.allocate();
 		Microphone microphone = (Microphone) cm.lookup("microphone");
@@ -18,17 +18,16 @@ public class SpeechRecognizer {
 			recognizer.deallocate();
 			System.exit(1);
 		}
-
-		System.out.println("Start speaking. Press Ctrl-C to quit.\n");
-		Result result = recognizer.recognize();
-		if (result != null) {
-			String resultText = result.getBestFinalResultNoFiller();
-			System.out.println("You said: " + resultText + '\n');
-		} else {
-			System.out.println("I can't hear what you said.\n");
+		while (true) {
+			System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+			Result result = recognizer.recognize();
+			if (result != null) {
+				String resultText = result.getBestFinalResultNoFiller();
+				System.out.println("You said: " + resultText + '\n');
+			} else {
+				System.out.println("I can't hear what you said.\n");
+			}
 		}
-
-		return null;
 	}
 
 	public static void main(String[] args) {
